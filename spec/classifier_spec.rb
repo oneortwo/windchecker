f610e3f8d3a2e7dfefe25d7ffa7270654048131c
Theodor Sjostedt <theodor.sjostedt@gmail.com>
2013-05-08 23:24:14 +0200

    deleted unnessesary newlines

--- spec/classifier_spec.rb
+++ spec/classifier_spec.rb
@@ -1,153 +1,77 @@
 require 'classifier'
-
 require 'weather'
-
-
 
 describe Classifier, "#apply" do
 
   it "returns cold-drizzle when f f f" do
-
     c = Classifier.new
-
     w = double("Weather")
-
     w.stub(:is_warm) { false }
-
     w.stub(:is_sunny) { false }
-
     w.stub(:is_windy) { false }
-
     c.apply(w).should eq("cold-drizzle")
-
   end
-
-
 
   it "returns storm when f f t" do
-
     c = Classifier.new
-
     w = double("Weather")
-
     w.stub(:is_warm) { false }
-
     w.stub(:is_sunny) { false }
-
     w.stub(:is_windy) { true }
-
     c.apply(w).should eq("storm")
-
   end
-
-
 
   it "returns winter-wonderland when f t f" do
-
     c = Classifier.new
-
     w = double("Weather")
-
     w.stub(:is_warm) { false }
-
     w.stub(:is_sunny) { true }
-
     w.stub(:is_windy) { false }
-
     c.apply(w).should eq("winter-wonderland")
-
   end
-
-
 
   it "returns winter-sailing when f t t" do
-
     c = Classifier.new
-
     w = double("Weather")
-
     w.stub(:is_warm) { false }
-
     w.stub(:is_sunny) { true }
-
     w.stub(:is_windy) { true }
-
     c.apply(w).should eq("winter-sailing")
-
   end
-
-
 
   it "returns muggy when t f f" do
-
     c = Classifier.new
-
     w = double("Weather")
-
     w.stub(:is_warm) { true }
-
     w.stub(:is_sunny) { false }
-
     w.stub(:is_windy) { false }
-
     c.apply(w).should eq("muggy")
-
   end
-
-
 
   it "returns hardcore-sailing when t f t" do
-
     c = Classifier.new
-
     w = double("Weather")
-
     w.stub(:is_warm) { true }
-
     w.stub(:is_sunny) { false }
-
     w.stub(:is_windy) { true }
-
     c.apply(w).should eq("hardcore-sailing")
-
   end
-
-
 
   it "returns laid-back when t t f" do
-
     c = Classifier.new
-
     w = double("Weather")
-
     w.stub(:is_warm) { true }
-
     w.stub(:is_sunny) { true }
-
     w.stub(:is_windy) { false }
-
     c.apply(w).should eq("laid-back")
-
   end
-
-
 
   it "returns perfect when t t t" do
-
     c = Classifier.new
-
     w = double("Weather")
-
     w.stub(:is_warm) { true }
-
     w.stub(:is_sunny) { true }
-
     w.stub(:is_windy) { true }
-
     c.apply(w).should eq("perfect")
-
   end
-
-
-
 end
