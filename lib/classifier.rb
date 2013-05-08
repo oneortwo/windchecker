c9e47bd0ccb3cf8f1a346578fef2565d265ef9a0
oneortwo <theodor.sjostedt@gmail.com>
2013-05-08 14:23:45 +0300

    Update classifier.rb

--- lib/classifier.rb
+++ lib/classifier.rb
@@ -1,7 +1,21 @@
 class Classifier
-  
+
+  def initialize
+    @classifications = {}
+    @classifications[ warm:false, sunny:false, windy:false ] = "cold-drizzle"
+    @classifications[ warm:false, sunny:false,  windy:true ] = "storm"
+    @classifications[ warm:false, sunny:true,  windy:false ] = "winter-wonderland"
+    @classifications[ warm:false, sunny:true,  windy:true ] = "winter-sailing"
+    @classifications[ warm:true, sunny:false,  windy:false ] = "muggy"
+    @classifications[ warm:true, sunny:false,  windy:true ] = "hardcore-sailing"
+    @classifications[ warm:true, sunny:true,  windy:false ] = "laid-back"
+    @classifications[ warm:true, sunny:true,  windy:true ] = "perfect"
+  end
+
   def apply(weather)
-    "cold-drizzle"
+    @classifications[ warm:weather.is_warm, 
+                      sunny:weather.is_sunny, 
+                      windy:weather.is_windy ]
   end
 
 end
