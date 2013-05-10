4e22b2b304d2f074811c0554bd88cd493327c37f
Theodor Sjostedt <theodor.sjostedt@gmail.com>
2013-05-10 16:39:18 +0200

    changed to use http get for weather data

--- app.rb
+++ app.rb
@@ -3,18 +3,21 @@ require 'bundler/setup'
 
 require 'sinatra'
 require 'haml'
+require 'net/http'
 require_relative 'lib/weather'
 require_relative 'lib/xml_reader'
 require_relative 'lib/populator'
 require_relative 'lib/classifier'
 
+$reader = XmlReader.new
+$populator = Populator.new
+$classifier = Classifier.new
+$uri = URI('http://www.yr.no/place/Sweden/Stockholm/Stockholm/forecast.xml')
+
 get '/' do
-  reader = XmlReader.new
-  p = Populator.new
-  c = Classifier.new
-  # replace with http get
-  xml = reader.read('forecast.xml')
-  weather = p.populate(xml)
-  @desc = c.apply(weather)
-  haml :index 
-end  
+  forecast = Net::HTTP.get($uri)
+  xml = $reader.read(forecast)
+  weather = $populator.populate(xml)
+  @desc = $classifier.apply(weather)
+  haml :index
+end
